485605b170b85c4bba29bda8aa101d8215bc6b93
jlocke817 <jlocke817@gmail.com>
2016-01-24 09:25:17 -0500

    Задание №3

---
+++ data.py
@@ -0,0 +1,10 @@
+class Data:
+    def __init__(self, firstname, lastname, nickname, title, company, home_phone, mobile_phone, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.email = email
